5d9aef867f7f99b4743a7fafa97e3e6f1c8321c7
ThierryConstance <thierry_constance@hotmail.fr>
2017-01-05 22:50:06 +0100

    Add files via upload

---
+++ pannes.js
@@ -0,0 +1,9 @@
+function actualisationFunction(mydiv) {
+	division = mydiv;
+	division.innerHTML = "Panne résolue";
+	division.style.backgroundColor = "#4CAF50";
+}
+
+/*TODO*/
+validation1panne(this){}
+validationpannes(this){}
